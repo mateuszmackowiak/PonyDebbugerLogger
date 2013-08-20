07e82c04fab2d09f037db3cc83c297ee58ff75e5
Mateusz Mackowiak <m@mateuszmackowiak.art.pl>
2013-08-20 22:09:00 +0200

    Initial Commit

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CocoaLumberjack
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack
+#define COCOAPODS_VERSION_MAJOR_CocoaLumberjack 1
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack 6
+#define COCOAPODS_VERSION_PATCH_CocoaLumberjack 2
+
+// PonyDebugger
+#define COCOAPODS_POD_AVAILABLE_PonyDebugger
+#define COCOAPODS_VERSION_MAJOR_PonyDebugger 0
+#define COCOAPODS_VERSION_MINOR_PonyDebugger 3
+#define COCOAPODS_VERSION_PATCH_PonyDebugger 0
+
+// SocketRocket
+#define COCOAPODS_POD_AVAILABLE_SocketRocket
+#define COCOAPODS_VERSION_MAJOR_SocketRocket 0
+#define COCOAPODS_VERSION_MINOR_SocketRocket 3
+#define COCOAPODS_VERSION_PATCH_SocketRocket 1
+
--- PonyDebuggerLoggerTest/MMViewController.m
+++ PonyDebuggerLoggerTest/MMViewController.m
@@ -17,13 +17,29 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	DDLogCError(@"YAhoo");
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)errorButtonTapped:(id)sender {
+    DDLogCError(@"Error button tapped");
+}
+
+- (IBAction)infoButtonTapped:(id)sender {
+    DDLogCInfo(@"Info button tapped");
+}
+
+
+- (IBAction)debugButtonTapped:(id)sender {
+    DDLogCVerbose(@"Debug button tapped");
+}
+
+- (IBAction)warnButtonTapped:(id)sender {
+    DDLogCWarn(@"Warning button tapped");
 }
 
 @end
